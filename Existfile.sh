4c75ebde977858b40aa5a4f53dcc04d9a22318e5
Sebuhi Gurbanov <qusabuhi15631@sabah.edu.az>
2020-01-30 21:10:09 +0300

    First Changes

---
+++ Existfile.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "########################################"
+
+if  [ -r /c/Users/Tux/Desktop/Bash-Repo/Bash.sh ] && [ -s /c/Users/Tux/Desktop/Bash-Repo/Bash.sh ]
+then
+    echo " this file is exist and is not empty "
+else
+    echo " This file is exist but is empty"
+fi 
+echo "########################################"l
