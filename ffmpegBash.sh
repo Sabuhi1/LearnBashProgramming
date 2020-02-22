4f608e36f73a4fc659ef224b8d2e8bf16787a15f
Sebuhi Gurbanov <qusabuhi15631@sabah.edu.az>
2020-02-22 23:37:22 +0300

    Names changes to input/output.mp4

--- ffmpegBash.sh
+++ ffmpegBash.sh
@@ -3,10 +3,17 @@
 cd C:\Ffmpegssds > /dev/null 2>&1
 if [ $? -eq 0 ];
 then
-    read -p "Write your ffmpeg code : ( e.g ffmpeg -i learning.mp4 -vn sabuhi.mp4 ) :  "
+    read -p "Write your ffmpeg code : ( e.g ffmpeg -i input.mp4 -vn output.mp4 ) :  "
 else
     VALUE=$(read -p " The process is failed! Do you want to continue ? Y/n")
     if [ $VALUE = Y ];then
     echo "Sorry.Again execute please!"
     fi 
 fi 
+
+
+
+
+
+
+
